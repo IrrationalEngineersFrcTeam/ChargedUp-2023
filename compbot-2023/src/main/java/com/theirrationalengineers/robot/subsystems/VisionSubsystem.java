9ce3cb03a0fbb98b292c2734f9524c5931ee95be
Jude Johnson <judej6706@gmail.com>
2023-03-01 19:26:13 -0500

    Cleaned up code

---
+++ compbot-2023/src/main/java/com/theirrationalengineers/robot/subsystems/VisionSubsystem.java
@@ -0,0 +1,13 @@
+package com.theirrationalengineers.robot.subsystems;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class VisionSubsystem extends SubsystemBase {
+    public VisionSubsystem() {}
+
+    @Override
+    public void periodic() {}
+
+    @Override
+    public void simulationPeriodic() {}
+}
